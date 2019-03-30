48c3be52ef42b845a4e02646c63082c3438e1d9d
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-03-30 20:18:28 +0100

    Local variables added in case of some errors fixes

--- src/components/BubbleSort.vue
+++ src/components/BubbleSort.vue
@@ -7,7 +7,7 @@
                         <h2 class="m-0">Bubble sort</h2>
                     </div>
                     <div class="col-6 text-right">
-                        <button class="btn btn-outline-dark mr-2" @click="initialize">
+                        <button class="btn btn-outline-dark mr-2" @click="initialize" v-show="!sorting">
                             Randomize
                         </button>
                         <button class="btn btn-info" @click="bubbleSort">
@@ -26,7 +26,7 @@
                 <div class="chart">
                     <div class="stick"
                          :class="{'bg-warning': index === checking, 'bg-info': index === actual, 'bg-success': item.done}"
-                         :style="{'height': item.value + '%', 'width': 100 / size + '%'}"
+                         :style="{'height': item.value + '%', 'width': 100 / saved.size + '%'}"
                          v-for="(item, index) in tab"></div>
                 </div>
             </div>
@@ -46,6 +46,7 @@
                 sorting: false,
                 saved: {
                     size: '',
+                    speed: '',
                 }
             }
         },
@@ -59,25 +60,28 @@
         },
         methods: {
             initialize() {
-                let generated = [];
-                for (let i = 0; i < this.size; i++) {
-                    generated[i] = {
-                        id: i,
-                        value: Math.floor(Math.random() * 100),
-                        done: false
-                    };
+                if (!this.sorting) {
+                    let generated = [];
+                    for (let i = 0; i < this.size; i++) {
+                        generated[i] = {
+                            id: i,
+                            value: Math.floor(Math.random() * 100),
+                            done: false
+                        };
+                    }
+                    this.tab = generated;
+                    this.saved.size = this.size;
+                    this.saved.speed = this.speed;
                 }
-                this.tab = generated;
-                this.saved.size = this.size;
             },
             bubbleSort() {
                 if (!this.sorting) {
                     this.sorting = true;
-                    let time = -(this.speed * this.size);
-                    for (let i = 0; i < this.size; i++) {
-                        time += (this.speed * (this.size - i));
+                    let time = -(this.saved.speed * this.saved.size);
+                    for (let i = 0; i < this.saved.size; i++) {
+                        time += (this.saved.speed * (this.saved.size - i));
                         setTimeout(() => {
-                            for (let j = 0; j < this.size - i - 1; j++) {
+                            for (let j = 0; j < this.saved.size - i - 1; j++) {
                                 setTimeout(() => {
                                     this.checking = j;
                                     this.actual = j + 1;
@@ -86,16 +90,16 @@
                                         this.$set(this.tab, j, this.tab[j + 1]);
                                         this.$set(this.tab, j + 1, tmp);
                                     }
-                                    if (j + 1 === this.size - i - 1) {
+                                    if (j + 1 === this.saved.size - i - 1) {
                                         this.tab[j + 1].done = true;
                                     }
-                                    if (this.size - i === 2) {
+                                    if (this.saved.size - i === 2) {
                                         this.tab[0].done = true;
                                         this.checking = -1;
                                         this.actual = -1;
                                         this.sorting = false;
                                     }
-                                }, j * this.speed);
+                                }, j * this.saved.speed);
                             }
                         }, time);
                     }
