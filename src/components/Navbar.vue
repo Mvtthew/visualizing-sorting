3daa7b6176dd29786799ac47346fb4c9511d5c8d
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-03-30 21:13:45 +0100

    navbar dark

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <router-link to="/" class="navbar-brand" href="#">
                 Visualizing Sorting
