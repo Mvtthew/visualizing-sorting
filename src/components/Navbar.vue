8c27bb7cb260b0474972c620950294a054bb59d3
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-03-30 17:05:55 +0100

    Adding navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                Visualizing Sorting
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <router-link to="/" class="nav-item">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </router-link>
+                    <router-link to="/about" class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </router-link>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'navbar'
+    }
+</script>
